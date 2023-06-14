0ec7bb64f2f6cb8c80d331294ec4bf2b5fbef1a7
Burapha Suriwan <burapha.s@pacgroup.local>
2023-06-14 16:22:54 +0700

    second edit

--- Booking/src/component/Page/ReportPage.jsx
+++ Booking/src/component/Page/ReportPage.jsx
@@ -30,8 +30,8 @@ function Copyright(props) {
   return (
     <Typography variant="body2" color="text.secondary" align="center" {...props}>
       {'Copyright © '}
-      <Link color="inherit" href="https://mui.com/">
-        Your Website
+      <Link color="inherit" href="https://www.facebook.com/icee5555">
+        by Burapha.S
       </Link>{' '}
       {new Date().getFullYear()}
       {'.'}
