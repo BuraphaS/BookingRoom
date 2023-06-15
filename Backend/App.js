085f8e8583ca2ea1d1432aa2cbc5c1cba4c82a78
Burapha Suriwan <burapha.s@pacgroup.local>
2023-06-15 11:59:11 +0700

    add second

--- Backend/App.js
+++ Backend/App.js
@@ -9,14 +9,10 @@ const saltRounds = 10
 const jwt = require('jsonwebtoken');
 const verifyToken = require('./middleware/auth')
 const secret = 'BackLogin'
-
+require('dotenv').config()
 
 const mysql = require('mysql2');
-const connection = mysql.createConnection({
-    host:'localhost',
-    user:'root',
-    database:'mydb'
-})
+const connection = mysql.createConnection(process.env.DATABASE_URL)
 
 app.use(cors())
 app.use(express.json())
